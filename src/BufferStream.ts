0a906a394f38d8247106c39c724a77040e6d4343
jakeFeldman <jacob.h.feldman@gmail.com>
2021-12-05 13:52:37 -0700

    rebuild with typescript

---
+++ src/BufferStream.ts
@@ -0,0 +1,42 @@
+import stream from 'stream';
+
+class BufferStream extends stream.Readable {
+    private _source: Buffer|null
+    private _offset: number|null
+    private _length: number|null
+
+    constructor(source: Buffer) {
+        super()
+        if (!Buffer.isBuffer(source)) {
+            throw (new Error('Source must be a buffer.'));
+        }
+
+        stream.Readable.call(this)
+
+        this._source = source;
+        this._offset = 0;
+        this._length = source.length;
+        this.on('end', this.destroy);
+    }
+
+    read (size: number) {
+        if (!this._source || !this._offset || !this._length) return
+        if (this._offset < this._length) {
+            this.push(this._source.slice(this._offset, (this._offset + size)));
+            this._offset += size;
+        }
+    
+        if (this._offset >= this._length) {
+            this.push(null);
+        }
+    }
+
+    destroy() {
+        this._source = null;
+        this._offset = null;
+        this._length = null;
+    }
+}
+
+
+export { BufferStream }
---
+++ src/index.ts
@@ -0,0 +1,95 @@
+import { BlobServiceClient } from '@azure/storage-blob'
+
+import { BufferStream } from './BufferStream';
+
+const trimParam = (str: string) => (typeof str === 'string' ? str.trim() : undefined);
+
+export default {
+    provider: 'azure',
+    auth: {
+        account: {
+            label: 'Account name',
+            type: 'text',
+        },
+        accountKey: {
+            label: 'Secret Access Key',
+            type: 'text',
+        },
+        serviceBaseURL: {
+            label: 'Base service URL to be used, optional. Defaults to https://${account}.blob.core.windows.net',
+            type: 'text',
+        },
+        containerName: {
+            label: 'Container name',
+            type: 'text',
+        },
+        defaultPath: {
+            label: 'The path to use when there is none being specified',
+            type: 'text',
+        },
+        maxConcurent: {
+            label: 'The maximum concurrent uploads to Azure',
+            type: 'number'
+        },
+        cdnBaseURL: {
+            label: 'Expose public CDN URL instead of service URL',
+            type: 'text',
+        },
+    },
+    init: config => {
+        const account = trimParam(config.account);
+        const accountKey = trimParam(config.accountKey);
+        // const sharedKeyCredential = new SharedKeyCredential(account, accountKey);
+        // const pipeline = StorageURL.newPipeline(sharedKeyCredential);
+        const serviceBaseURL = trimParam(config.serviceBaseURL) || `https://${account}.blob.core.windows.net`
+        // const serviceURL = new ServiceURL(serviceBaseURL, pipeline);
+        // const containerURL = ContainerURL.fromServiceURL(serviceURL, config.containerName);
+        const cdnBaseURL = trimParam(config.cdnBaseURL);
+
+        const connStr = '';
+        const client = BlobServiceClient.fromConnectionString(connStr);
+
+        return {
+            upload: file => new Promise((resolve, reject) => {
+                const fileName = file.hash + file.ext;
+                // const containerWithPath = Object.assign({}, containerURL);
+                // containerWithPath.url += file.path ? `/${file.path}` : `/${config.defaultPath}`;
+
+                // const blobURL = BlobURL.fromContainerURL(containerWithPath, fileName);
+                // const blockBlobURL = BlockBlobURL.fromBlobURL(blobURL);
+
+                // file.url = cdnBaseURL 
+                //     ? blobURL.url.replace(serviceBaseURL, cdnBaseURL)
+                //     : blobURL.url;
+
+                // return uploadStreamToBlockBlob(
+                //     Aborter.timeout(60 * 60 * 1000),
+                //     new BufferStream(file.buffer), blockBlobURL,
+                //     4 * 1024 * 1024,
+                //     ~~(config.maxConcurent) || 20,
+                //     {
+                //         blobHTTPHeaders: {
+                //             blobContentType: file.mime
+                //         }
+                //     }
+                // ).then(resolve, reject);
+            }),
+            delete: (file: { url: string }) => new Promise((resolve, reject) => {
+                let fileUrl = file.url;
+                if (cdnBaseURL) {
+                    fileUrl = fileUrl.replace(cdnBaseURL, serviceBaseURL);
+                }
+                const _temp = fileUrl.replace(containerURL.url, '');
+                const pathParts = _temp.split('/').filter(x => x.length > 0);
+                const fileName = pathParts.splice(pathParts.length - 1, 1);
+                const containerWithPath = Object.assign({}, containerURL);
+                containerWithPath.url += '/' + pathParts.join('/');
+
+                // const blobURL = BlobURL.fromContainerURL(containerWithPath, fileName);
+                // const blockBlobURL = BlockBlobURL.fromBlobURL(blobURL);
+
+                // return blockBlobURL.delete().then(resolve, (err: Error) => reject(err));
+            }),
+        };
+    }
+};
